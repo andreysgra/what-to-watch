64477adb5d318424d0f36f0914a86c914f2d2ade
Andrey Grachev <andreysgra@gmail.com>
2026-02-02 22:50:43 +0300

    Добавляет маршрутизацию в приложении.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,47 @@
 import MainPage from '../../pages/main-page/main-page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import LoginPage from '../../pages/login-page/login-page';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import FilmPage from '../../pages/film-page/film-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import AddReviewPage from '../../pages/add-review-page/add-review-page';
 
 type AppProps = {
   filmsCount: number;
 }
 
 function App({filmsCount}: AppProps) {
-  return <MainPage filmsCount={filmsCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={<MainPage filmsCount={filmsCount} /> }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListPage />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmPage />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewPage />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
