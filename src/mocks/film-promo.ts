02957da75561680367dc60040c4febabef68d34e
Andrey Grachev <andreysgra@gmail.com>
2026-02-06 20:24:22 +0300

    Создаёт тестовые данные для промо-фильма и детального описания фильма.

---
+++ src/mocks/film-promo.ts
@@ -0,0 +1,12 @@
+import {TFilmPromo} from '../types/film';
+
+export const filmPromo: TFilmPromo = {
+  'id': '3cd995e7-b6b1-4a07-8abf-7e8e01207d68',
+  'name': 'Fantastic Beasts: The Crimes of Grindelwald',
+  'posterImage': 'https://16.design.htmlacademy.pro/static/film/poster/Fantastic_Beasts.jpg',
+  'backgroundImage': 'https://16.design.htmlacademy.pro/static/film/background/Fantastic_Beasts.jpg',
+  'videoLink': 'https://16.design.htmlacademy.pro/static/film/video/matrix.mp4',
+  'genre': 'Fantasy',
+  'released': 2018,
+  'isFavorite': true
+};
