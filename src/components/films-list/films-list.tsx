a0b0b6d2e5433d19443b2e31fa44e6decfb5acaa
Andrey Grachev <andreysgra@gmail.com>
2026-02-09 17:36:12 +0300

    Подключает компонент видеоплеера в карточке фильма.

--- src/components/films-list/films-list.tsx
+++ src/components/films-list/films-list.tsx
@@ -7,16 +7,17 @@ type FilmsListProps = {
 }
 
 function FilmsList({films}: FilmsListProps) {
-  const [, setActiveOfferId] = useState<string | null>(null);
+  const [filmCurrentId, setFilmCurrentId] = useState<string | null>(null);
 
-  const handleCardMouseEnter = (id: string) => setActiveOfferId(id);
-  const handleCardMouseLeave = () => setActiveOfferId(null);
+  const handleCardMouseEnter = (id: string) => setFilmCurrentId(id);
+  const handleCardMouseLeave = () => setFilmCurrentId(null);
 
   return (
     <div className="catalog__films-list">
       {films.map((film) => (
         <FilmCard
           key={film.id} film={film}
+          filmCurrentId={filmCurrentId}
           onMouseEnter={handleCardMouseEnter}
           onMouseLeave={handleCardMouseLeave}
         />
