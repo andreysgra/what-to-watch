020714e6f402e62dc5bdb3e2c957d9c93199f745
Andrey Grachev <andreysgra@gmail.com>
2026-02-11 19:08:26 +0300

    Создаёт тестовые данные отзывов о фильме.

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,26 @@
+import {TReviews} from '../types/review';
+
+export const reviews: TReviews = [
+  {
+    'id': '77e30e50-00c6-45bd-98ee-5000a29163d8',
+    'comment': 'It was well acted, directed, and the music was good. But the story is yawn. Not trying to rip anybody but I checked my watch a dozen times during this movie.',
+    'date': '2026-01-18T21:00:00.134Z',
+    'rating': 5,
+    'user': 'Emely'
+  },
+  {
+    'id': 'f2d56c65-4363-4af1-a56a-25118323ed8e',
+    'comment': 'This movie is just plain bad. There must be some big payola going round this awards season. Badly written, average acting at best, all the characters are unrelatable and inlikeable. 2 hours of my life wasted.',
+    'date': '2026-01-16T21:00:00.134Z',
+    'rating': 7,
+    'user': 'Jack'
+  },
+  {
+    'id': '6e5d8550-89ab-4050-9a97-fa9938c643f4',
+    'comment': 'This movie really touched my heart, it really is the best movie of the year and everyone should see this masterpiece.',
+    'date': '2026-01-15T21:00:00.134Z',
+    'rating': 10,
+    'user': 'Sophie'
+  }
+];
+
