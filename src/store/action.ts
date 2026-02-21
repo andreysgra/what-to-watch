af282af56d982e34598252d3373660687be6224e
Andrey Grachev <andreysgra@gmail.com>
2026-02-21 14:58:53 +0300

    Создаёт действие для количества отображаемых фильмов на странице.

--- src/store/action.ts
+++ src/store/action.ts
@@ -4,3 +4,5 @@ import {TFilms} from '../types/film';
 export const setGenre = createAction<string>('genre/set');
 
 export const setFilms = createAction<TFilms>('films/set');
+
+export const setFilmsCount = createAction<number>('filmsCount/set');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,16 +1,18 @@
 import {TFilms} from '../types/film';
 import {createReducer} from '@reduxjs/toolkit/src';
-import {setFilms, setGenre} from './action';
-import {ALL_GENRES} from '../const';
+import {setFilms, setFilmsCount, setGenre} from './action';
+import {ALL_GENRES, FILMS_PER_LOAD} from '../const';
 
 type State = {
   genre: string;
   films: TFilms;
+  filmsCount: number;
 }
 
 const initialState: State = {
   genre: ALL_GENRES,
-  films: []
+  films: [],
+  filmsCount: FILMS_PER_LOAD
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -20,5 +22,8 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setFilms, (state, action) => {
       state.films = action.payload;
+    })
+    .addCase(setFilmsCount, (state, action) => {
+      state.filmsCount = action.payload;
     });
 });
