95037a52f49ce1db661914eec3ade7580f941c09
Andrey Grachev <andreysgra@gmail.com>
2026-02-21 15:55:13 +0300

    Реализует логику количества фильмов, отображаемых на странице.

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,12 +1,18 @@
 import GenresList from '../genres-list/genres-list';
 import FilmsList from '../films-list/films-list';
 import {useAppSelector} from '../../hooks/use-app-selector';
-import {ALL_GENRES, MAX_GENRES_COUNT} from '../../const';
+import {ALL_GENRES, FILMS_PER_LOAD, MAX_GENRES_COUNT} from '../../const';
 import {groupBy} from '../../utils/utils';
+import ShowMoreButton from '../show-more-button/show-more-button';
+import {useAppDispatch} from '../../hooks/use-app-dispatch';
+import {setFilmsCount} from '../../store/action';
 
 function Catalog() {
   const films = useAppSelector((state) => state.films);
   const activeGenre = useAppSelector((state) => state.genre);
+  const displayedFilms = useAppSelector((state) => state.filmsCount);
+
+  const dispatch = useAppDispatch();
 
   const filmsGroupedByGenre = groupBy(films, (film) => film.genre);
 
@@ -17,16 +23,18 @@ function Catalog() {
   const filmsByGenre =
     (activeGenre === ALL_GENRES) ? films : films.filter((film) => film.genre === activeGenre);
 
+  const handleShowMoreButtonClick = () => dispatch(setFilmsCount(displayedFilms + FILMS_PER_LOAD));
+
   return (
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
       <GenresList genres={genres} />
-      <FilmsList films={filmsByGenre} />
-      <div className="catalog__more">
-        <button className="catalog__button" type="button">
-          Show more
-        </button>
-      </div>
+      <FilmsList films={filmsByGenre.slice(0, displayedFilms)} />
+      {filmsByGenre.length > displayedFilms && (
+        <div className="catalog__more">
+          <ShowMoreButton onClick={handleShowMoreButtonClick} />
+        </div>
+      )}
     </section>
   );
 }
