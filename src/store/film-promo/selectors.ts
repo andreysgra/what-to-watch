454ba49f2027370ba6f4e6fbc970c08d8bd73158
Andrey Grachev <andreysgra@gmail.com>
2026-03-26 22:10:33 +0300

    Делает рефакторинг компонента промо-фильма.

--- src/store/film-promo/selectors.ts
+++ src/store/film-promo/selectors.ts
@@ -1,7 +1,13 @@
 import {State} from '../../types/state';
 import {StoreSlice} from '../const';
+import {RequestStatus} from '../../services/api/const';
 
-export const getFilmPromo = (state: State) => state[StoreSlice.FilmPromo].filmPromo;
+const storeSlice = StoreSlice.FilmPromo;
+
+export const getFilmPromo = (state: State) => state[storeSlice].filmPromo;
 
 export const getIsFilmPromoLoading = (state: State) =>
-  state[StoreSlice.FilmPromo].isFilmPromoLoading;
+  state[storeSlice].loadingStatus === RequestStatus.Pending;
+
+export const getIsFilmPromoFailed = (state: State)=>
+  state[storeSlice].loadingStatus === RequestStatus.Error;
